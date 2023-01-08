37a97e427a1717333c2bdf7c46b1a651342b4506
subminu <subminu@gmail.com>
2023-01-08 08:41:59 +0000

    add more parameter for new version, hydra 1.3

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import hydra
 from omegaconf import DictConfig
 
 
-@hydra.main(config_path="configs", config_name="config")
+@hydra.main(version_base=None, config_path="./conf", config_name="main")
 def main(config: DictConfig):
     from src.train import train
 
