d91c9a94558ecc6dfe2ac0d18b85d28de683e88f
subminu <subminu@gmail.com>
2023-01-09 03:55:29 +0000

    delete set_random_seed fucntion it will be integrated to trainer class

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -53,15 +53,6 @@ class SetDistibutedFunc(object):
 # --------- decorator --------- #
 
 
-def set_seed(random_seed: int) -> None:
-    torch.manual_seed(random_seed)
-    torch.cuda.manual_seed(random_seed)
-    torch.cuda.manual_seed_all(random_seed)  # if use multi-GPU
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
-    np.random.seed(random_seed)
-    random.seed(random_seed)
-
 
 def get_logger(name=__name__) -> logging.Logger:
     logger = logging.getLogger(name)
